ada78b749e93661494b9133beee636ad0ae47c8c
404232077 <40423207@gm.nfu.edu.tw>
2019-07-19 13:59:07 +0800

    19/07/19

---
+++ program4/prog4_HW_10.cpp
@@ -0,0 +1,13 @@
+/* prog4_HW_10 */
+# include<stdio.h>
+# include<stdlib.h>
+int main (void)
+{
+	int x,y;
+	printf("pleasr enter 2 integer:");
+	scanf("%d %d",&x,&y);
+	printf("x*y=%d\n",x*y);
+	
+	system ("pause");
+	return 0;	
+}
---
+++ program4/prog4_HW_12.cpp
@@ -0,0 +1,17 @@
+/* prog4_HW_12 */
+# include<stdio.h>
+# include<stdlib.h>
+int main (void)
+{
+	char first[10],last[10];
+	printf("pleae enter your fist name:");
+	scanf("%s",first);
+	
+	printf("pleae enter your last name:");
+	scanf("%s",last);
+	
+	printf("your name is:%s,%s\n",last,first);
+	
+	system ("pause");
+	return 0;	
+}
